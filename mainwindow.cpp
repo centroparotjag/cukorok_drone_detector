f717b65a8b608bd8060ce4541ea34f1eaacb033a
centroparotjag <centroparotjag@gmail.com>
2023-09-10 18:46:12 +0300

    add functional

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,7 +11,14 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui -> pushButton -> setText("З'єднати");
-    on_pushButton_5_clicked();              // refresh com ports
+    //ui -> comboBox_2 -> setEnabled(0);        // відключений
+    ui -> groupBox_2 -> setEnabled(0);          // відключений
+    ui -> groupBox_4 -> setEnabled(0);          // конфігурація відключений
+    ui -> groupBox_9 -> setEnabled(0);          // команда (comboBox_2) відключений
+    ui -> groupBox_10 -> setEnabled(0);         // WI-FI термінал відключений
+    ui->textBrowser->setStyleSheet("background: black");
+    ui->textBrowser->setFontPointSize(10);
+    on_pushButton_5_clicked();                  // refresh com ports
 }
 
 MainWindow::~MainWindow()
@@ -25,6 +32,7 @@ MainWindow::~MainWindow()
 void MainWindow::serialRecieve(){
     QByteArray ba;
     ba = serial -> readAll();
+    ui->textBrowser->setTextColor(QColor(0, 255, 0));
     ui -> textBrowser -> append(ba);
 }
 
@@ -32,6 +40,9 @@ void MainWindow::serialRecieve(){
 void MainWindow::on_pushButton_5_clicked()      // оновити доступні com ports
 {
     ui -> comboBox -> clear();
+    ui->textBrowser->setTextColor(QColor(102, 178, 255));
+    ui -> textBrowser -> append("\n-----------------------------------------");
+    ui -> textBrowser -> append("Доступні для підключення порти:");
 
     foreach(const QSerialPortInfo &info, QSerialPortInfo::availablePorts() ){
         QSerialPort port;
@@ -39,9 +50,12 @@ void MainWindow::on_pushButton_5_clicked()      // оновити доступн
         if(port.open(QIODevice::ReadWrite))
         {
             qDebug() << "Назва - " + info.portName() + " " + info.description() + info.manufacturer();
+
+            ui -> textBrowser -> append(info.portName() + " " + info.description() + info.manufacturer());
             ui -> comboBox -> addItem(info.portName());
         }
     }
+    ui -> textBrowser -> append("-----------------------------------------\n");
 }
 
 
@@ -50,13 +64,12 @@ void MainWindow::on_pushButton_clicked()            // підключити/ві
         if (!status_connect_com){
             qDebug() << "Порт вільний, підключаюсь";
             serial = new QSerialPort (this);
-            serial -> setPortName(ui->comboBox->currentText());                   // вказуєм com port для підключення з combobox
+            serial -> setPortName(ui->comboBox->currentText());                // вказуєм com port для підключення з combobox
             serial -> setBaudRate(QSerialPort::Baud115200);
             serial -> setDataBits(QSerialPort::Data8);
             serial -> setParity(QSerialPort::NoParity);
             serial -> setStopBits(QSerialPort::OneStop);
             serial -> setFlowControl(QSerialPort::NoFlowControl);
-
 
             if (serial -> open(QIODevice::ReadWrite) ){    // відкриваєм порт або перевіряєм його зайнятість
                 qDebug() << "Під'єднано";
@@ -64,7 +77,15 @@ void MainWindow::on_pushButton_clicked()            // підключити/ві
                 ui -> groupBox ->setStyleSheet("QGroupBox {background-color:rgb(135, 223, 71);}");
                 ui -> pushButton_5 -> setEnabled(0);        // конопка оновити відключена
                 ui -> comboBox     -> setEnabled(0);        // список компортів відключена
+                ui -> groupBox_2 -> setEnabled(1);          // включений
+                //ui -> groupBox_4 -> setEnabled(1);          // включений
+                ui -> groupBox_9 -> setEnabled(1);          // список команд включений
+                ui -> groupBox_10 -> setEnabled(1);         // включений
                 ui -> comboBox     -> colorCount();
+
+                ui->textBrowser->setTextColor(QColor(255, 255, 0));
+                ui -> textBrowser -> append("Підключено до: " + ui->comboBox->currentText() + "\n");
+
             }
             connect(serial, SIGNAL(readyRead()), this, SLOT(serialRecieve()));
             status_connect_com = 1;
@@ -74,74 +95,19 @@ void MainWindow::on_pushButton_clicked()            // підключити/ві
             qDebug() << "Порт зайнято, відєднуюсь";
             ui -> pushButton   -> setText("З'єднати");
             ui -> groupBox ->setStyleSheet("QGroupBox {background-color:rgb(241, 241, 241);}");
-            ui -> pushButton_5 ->setEnabled(1);         // конопка оновити відключена
-            ui -> comboBox -> setEnabled(1);            // список компортів відключена
+            ui -> pushButton_5 ->setEnabled(1);         // конопка оновити включений
+            ui -> comboBox -> setEnabled(1);            // список компортів включений
+            ui -> groupBox_2 -> setEnabled(0);          // відключений
+            ui -> groupBox_4 -> setEnabled(0);          // відключений
+            ui -> groupBox_9 -> setEnabled(0);          // список команд відключений
+            ui -> groupBox_10 -> setEnabled(0);         // відключений
             status_connect_com = 0;
         }
 }
 
 
-void MainWindow::on_pushButton_8_clicked()
-{
-    serial -> write("status;");
-}
 
-void MainWindow::on_pushButton_12_clicked()
-{
-    serial -> write("restart;");
-}
-
-void MainWindow::on_pushButton_6_clicked()
-{
-    serial -> write("verbose 1;");
-}
-
-void MainWindow::on_pushButton_14_clicked()
-{
-    serial -> write("verbose 0;");
-}
-
-void MainWindow::on_pushButton_18_clicked()
-{
-    serial -> write("tracking enable;");
-}
-
-void MainWindow::on_pushButton_16_clicked()
-{
-    serial -> write("tracking disable;");
-}
-
-//test alarm <вид сигналізації orlan, eleron, rssi, tracking>
-void MainWindow::on_pushButton_7_clicked()
-{
-    serial -> write("test alarm orlan;");
-}
-
-void MainWindow::on_pushButton_9_clicked()
-{
-    serial -> write("test alarm eleron;");
-}
-
-void MainWindow::on_pushButton_11_clicked()
-{
-    serial -> write("test alarm rssi;");
-}
-
-void MainWindow::on_pushButton_17_clicked()
-{
-    serial -> write("test alarm tracking;");
-}
-
-void MainWindow::on_pushButton_13_clicked()
-{
-    serial -> write("update;"); //Оновити прошивку, якщо є новіша.
-}
-
-void MainWindow::on_pushButton_15_clicked()
-{
-    serial -> write("upload captures;"); //Завантажити накопичені сигнатури на мій сервер для аналізу.
-}
-
+//----------- WI-FI and update FW ----------------
 void MainWindow::on_pushButton_10_clicked()
 {
     QString ssid = ui->lineEdit->text();
@@ -149,29 +115,93 @@ void MainWindow::on_pushButton_10_clicked()
     QByteArray ba = "wifi client connect ";
     ba.append(ssid + " " + pass + ";");
     serial -> write(ba);
-
-    qDebug()<< ba;
+    ui->textBrowser->setTextColor(QColor(255, 0, 0));
+    ui -> textBrowser -> append("-->> wifi client connect "+ ssid + " " + pass + ";");
 }
 
 void MainWindow::on_pushButton_19_clicked()
 {
     serial -> write("wifi client disconnect;");
+    ui->textBrowser->setTextColor(QColor(255, 0, 0));
+    ui -> textBrowser -> append("-->> wifi client disconnect;");
 }
 
 void MainWindow::on_pushButton_20_clicked()
 {
     serial -> write("update;");
+    ui->textBrowser->setTextColor(QColor(255, 0, 0));
+    ui -> textBrowser -> append("-->> update;");
 }
+//----------------------------------------------------
 
-void MainWindow::on_pushButton_21_clicked()
-{
-    serial -> write("settings;");
-}
 
-void MainWindow::on_pushButton_22_clicked()
+void MainWindow::on_pushButton_22_clicked()   // write data from terminal
 {
     QString qs = ui->lineEdit_15->text();
     QByteArray ba=0;
     ba += qs;
     serial -> write(ba);
+    ui->textBrowser->setTextColor(QColor(255, 125, 125));
+    ui -> textBrowser -> append("-->> " + ba);
 }
+
+
+//------------------- combo box comands --------------------------
+void MainWindow::on_comboBox_2_activated(const QString &arg1)
+{
+    ui->textBrowser->setTextColor(QColor(255, 0, 0));
+    ui -> textBrowser -> append("-->> " + ui-> comboBox_2 -> currentText());
+
+    switch(ui-> comboBox_2 -> currentIndex()) {
+      case 0:
+        serial -> write("status;");
+        break;
+      case 1:
+        serial -> write("settings;");
+        break;
+      case 2:
+        serial -> write("restart;");
+        break;
+      case 3:
+        serial -> write("wifi client scan;");
+        break;
+      case 4:
+        serial -> write("update;");
+        break;
+      case 5:
+        serial -> write("upload captures;");
+        break;
+      case 6:
+        serial -> write("verbose 1;");
+        break;
+      case 7:
+        serial -> write("verbose 0;");
+        break;
+      case 8:
+        serial -> write("tracking enable;");
+        break;
+      case 9:
+        serial -> write("tracking disable;");
+        break;
+      case 10:
+        serial -> write("dji 1;");
+        break;
+      case 11:
+        serial -> write("dji 0;");
+        break;
+      case 12:
+        serial -> write("test alarm orlan;");
+        break;
+      case 13:
+        serial -> write("test alarm eleron;");
+        break;
+      case 14:
+        serial -> write("test alarm rssi;");
+        break;
+      case 15:
+       serial -> write("test alarm tracking;");
+       break;
+    }
+}
+
+
